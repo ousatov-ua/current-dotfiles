32613fc2a764ecfbf9ab275df36dc73e8e4f5695
Oleksii Usatov <usatovalexey81@gmail.com>
2022-01-08 10:37:39 +0200

    TreeSitter config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -44,7 +44,7 @@ opt.undodir = vim.fn.stdpath("config") .. "/undo"
 opt.undofile = true
 vim.cmd("au TextYankPost * lua vim.highlight.on_yank {on_visual = true}") -- disabled in visual mode
 -- Give me some fenced codeblock goodness
-vim.g.markdown_fenced_languages = { "html", "javascript", "typescript", "css", "scss", "lua", "vim" }
+-- vim.g.markdown_fenced_languages = { "html", "javascript", "typescript", "css", "scss", "lua", "vim" }
 vim.g.mapleader = ","
 
 vim.cmd([[
@@ -167,3 +167,28 @@ require('vgit').setup()
 
 -- Nvim tree
 require('nvim-tree').setup() -- yes, needed to properly setup commands
+
+require'nvim-treesitter.configs'.setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "maintained",
+
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing
+  -- ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    -- disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
