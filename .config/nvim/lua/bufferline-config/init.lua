de022bb866d56eaa98e884c91c23e8fe7f9a6ba3
Oleksii Usatov <usatovalexey81@gmail.com>
2022-03-10 20:12:47 +0200

    Updated go to buffer

--- .config/nvim/lua/bufferline-config/init.lua
+++ .config/nvim/lua/bufferline-config/init.lua
@@ -1,6 +1,12 @@
-require("bufferline").setup()
+require("bufferline").setup{
+    options = {
+        separator_style = "thick",
+        show_tab_indicators = true
+    }
+}
 
 vim.cmd([[
 nnoremap <C-l> :BufferLineCycleNext<CR>
 nnoremap <C-h> :BufferLineCyclePrev<CR>
+nnoremap <space>gb :BufferLinePick<CR>
 ]])
